4852f6227f984087ce7f41834653327cee172dbb
kmcc08 <kat_mccormick27@milton.edu>
2025-01-10 13:50:47 -0500

    commit

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -1,17 +1,3 @@
-//Basic Game Application
-//Version 2
-// Basic Object, Image, Movement
-// Astronaut moves to the right.
-// Threaded
-
-//K. Chun 8/2018
-
-//*******************************************************************************
-//Import Section
-//Add Java libraries needed for the game
-//import java.awt.Canvas;
-
-//Graphics Libraries
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 import java.awt.*;
@@ -24,117 +10,153 @@ import javax.swing.JPanel;
 
 public class BasicGameApp implements Runnable {
 
-   //Variable Definition Section
-   //Declare the variables used in the program 
-   //You can set their initial values too
-   
-   //Sets the width and height of the program window
+	//Variable Definition Section
+	//Declare the variables used in the program
+	//You can set their initial values too
+
+	//Sets the width and height of the program window
 	final int WIDTH = 1000;
 	final int HEIGHT = 700;
 
-   //Declare the variables needed for the graphics
+	//Declare the variables needed for the graphics
 	public JFrame frame;
 	public Canvas canvas;
-   public JPanel panel;
-   
+	public JPanel panel;
+
 	public BufferStrategy bufferStrategy;
-	public Image astroPic;
+	public Image DinosaurPic;
+	public Image badGuyPic;
+	public Image BackgroundPic;
+	public Image FirePic;
+	//Declare the objects used in the program
+	//These are things that are made up of more than one variable type
+	private Character Dinosaur;
+	private Character badGuy;
+	private Character Fire;
 
-   //Declare the objects used in the program
-   //These are things that are made up of more than one variable type
-	private Astronaut astro;
 
-
-   // Main method definition
-   // This is the code that runs first and automatically
+	// Main method definition
+	// This is the code that runs first and automatically
 	public static void main(String[] args) {
 		BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
-		new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method  
+		new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
 	}
 
 
-   // Constructor Method
-   // This has the same name as the class
-   // This section is the setup portion of the program
-   // Initialize your variables and construct your program objects here.
+	// Constructor Method
+	// This has the same name as the class
+	// This section is the setup portion of the program
+	// Initialize your variables and construct your program objects here.
 	public BasicGameApp() {
-      
-      setUpGraphics();
-       
-      //variable and objects
-      //create (construct) the objects needed for the game and load up 
-		astroPic = Toolkit.getDefaultToolkit().getImage("astronaut.png"); //load the picture
-		astro = new Astronaut(10,100);
+
+		setUpGraphics();
+
+		//variable and objects
+		//create (construct) the objects needed for the game and load up
+		DinosaurPic = Toolkit.getDefaultToolkit().getImage("dinosaur.png");
+		FirePic = Toolkit.getDefaultToolkit().getImage("fire.jpg");
+		badGuyPic = Toolkit.getDefaultToolkit().getImage("Screenshot 2024-12-18 115637.png");//load the picture
+		BackgroundPic = Toolkit.getDefaultToolkit().getImage("cave.png");
+		Dinosaur = new Character(10, 100);
+		badGuy = new Character(600, 600);
+		Fire = new Character(700, 350);
+		Fire.isAlive = false;
+		badGuy.isAlive = true;
 
 
 	}// BasicGameApp()
 
-   
+
 //*******************************************************************************
 //User Method Section
 //
 // put your code to do things here.
 
-   // main thread
-   // this is the code that plays the game after you set things up
+	// main thread
+	// this is the code that plays the game after you set things up
 	public void run() {
 
-      //for the moment we will loop things forever.
+		//for the moment we will loop things forever.
 		while (true) {
 
-         moveThings();  //move all the game objects
-         render();  // paint the graphics
-         pause(20); // sleep for 10 ms
+			moveThings();  //move all the game objects
+			render();  // paint the graphics
+			pause(20); // sleep for 10 ms
 		}
 	}
 
 
-	public void moveThings()
-	{
-      //calls the move( ) code in the objects
-		astro.move();
+	public void moveThings() {
+
+		//calls the move( ) code in the object
+		collisions();
+		Dinosaur.bounce();
+		badGuy.bounce();
+
 
 	}
-	
-   //Pauses or sleeps the computer for the amount specified in milliseconds
-   public void pause(int time ){
-   		//sleep
-			try {
-				Thread.sleep(time);
-			} catch (InterruptedException e) {
 
-			}
-   }
+	public void collisions() {
+		if (Dinosaur.rec.intersects(badGuy.rec) && Dinosaur.isCrashing == false) {
+			System.out.println(" explosion!!!!!!! ");
+			Dinosaur.dx = -Dinosaur.dx;
+			Dinosaur.dy = -Dinosaur.dy;
+			badGuy.dx = -badGuy.dx;
+			badGuy.dy = -badGuy.dy;
+			Dinosaur.isCrashing = true;
+			System.out.println("-1 life");
+			Fire.isAlive = true;
+			badGuy.isAlive = false;
 
-   //Graphics setup method
-   private void setUpGraphics() {
-      frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
-   
-      panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
-      panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
-      panel.setLayout(null);   //set the layout
-   
-      // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
-      // and trap input events (Mouse and Keyboard events)
-      canvas = new Canvas();  
-      canvas.setBounds(0, 0, WIDTH, HEIGHT);
-      canvas.setIgnoreRepaint(true);
-   
-      panel.add(canvas);  // adds the canvas to the panel.
-   
-      // frame operations
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
-      frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
-      frame.setResizable(false);   //makes it so the frame cannot be resized
-      frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
-      
-      // sets up things so the screen displays images nicely.
-      canvas.createBufferStrategy(2);
-      bufferStrategy = canvas.getBufferStrategy();
-      canvas.requestFocus();
-      System.out.println("DONE graphic setup");
-   
-   }
+		}
+		if (!Dinosaur.rec.intersects(badGuy.rec)) {
+			Dinosaur.isCrashing = false;
+		//	Fire.isAlive = false;
+
+		}
+
+
+	}
+
+	//Pauses or sleeps the computer for the amount specified in milliseconds
+	public void pause(int time) {
+		//sleep
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+
+		}
+	}
+
+	//Graphics setup method
+	private void setUpGraphics() {
+		frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
+
+		panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+		panel.setLayout(null);   //set the layout
+
+		// creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+		// and trap input events (Mouse and Keyboard events)
+		canvas = new Canvas();
+		canvas.setBounds(0, 0, WIDTH, HEIGHT);
+		canvas.setIgnoreRepaint(true);
+
+		panel.add(canvas);  // adds the canvas to the panel.
+
+		// frame operations
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+		frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+		frame.setResizable(false);   //makes it so the frame cannot be resized
+		frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+		// sets up things so the screen displays images nicely.
+		canvas.createBufferStrategy(2);
+		bufferStrategy = canvas.getBufferStrategy();
+		canvas.requestFocus();
+		System.out.println("DONE graphic setup");
+
+	}
 
 
 	//paints things on the screen using bufferStrategy
@@ -142,11 +164,19 @@ public class BasicGameApp implements Runnable {
 		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
 		g.clearRect(0, 0, WIDTH, HEIGHT);
 
-      //draw the image of the astronaut
-		g.drawImage(astroPic, astro.xpos, astro.ypos, astro.width, astro.height, null);
+		//draw the image of the Dinosaurnaut
+		g.drawImage(BackgroundPic, 0, 0, WIDTH, HEIGHT, null);
 
+		g.drawImage(DinosaurPic, Dinosaur.xpos, Dinosaur.ypos, Dinosaur.width, Dinosaur.height, null);
+		if (Fire.isAlive == true) {
+			g.drawImage(FirePic, Fire.xpos, Fire.ypos, Fire.width, Fire.height, null);
+		}
+        if (badGuy.isAlive == true){
+			g.drawImage(badGuyPic, badGuy.xpos, badGuy.ypos, badGuy.width, badGuy.height, null);
+		}
 		g.dispose();
 
 		bufferStrategy.show();
+
 	}
 }
---
+++ src/Character.java
@@ -0,0 +1,107 @@
+import java.awt.*;
+
+/**
+ * Created by chales on 11/6/2017.
+ */
+public class Character {
+
+    //VARIABLE DECLARATION SECTION
+    //Here's where you state which variables you are going to use.
+    public String name;                //holds the name of the hero
+    public int xpos;                //the x position
+    public int ypos;                //the y position
+    public int dx;                    //the speed of the hero in the x direction
+    public int dy;                    //the speed of the hero in the y direction
+    public int width;
+    public int height;
+    public boolean isAlive;//a boolean to denote if the hero is alive or dead.
+    public Rectangle rec;
+    public boolean isCrashing;
+
+
+    // METHOD DEFINITION SECTION
+
+    // Constructor Definition
+    // A constructor builds the object when called and sets variable values.
+
+
+    //This is a SECOND constructor that takes 3 parameters.  This allows us to specify the hero's name and position when we build it.
+    // if you put in a String, an int and an int the program will use this constructor instead of the one above.
+    public Character (int pXpos, int pYpos) {
+        xpos = pXpos;
+        ypos = pYpos;
+        dx =4;
+        dy =8;
+        width = 60;
+        height = 60;
+        isAlive = true;
+        rec  = new Rectangle(xpos, ypos, width, height);
+        isCrashing = false;
+
+    } // constructor
+
+    //The move method.  Everytime this is run (or "called") the hero's x position and y position change by dx and dy
+    public void move() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        rec  = new Rectangle(xpos, ypos, width, height);
+    }
+
+    public void bounce (){
+        if (xpos>1000) {
+            dx=-dx;
+        }
+
+        if (xpos<0) {
+            dx=-dx;
+        }
+        if (ypos>600) {
+            dy=-dy;
+        }
+
+        if (ypos<0) {
+            dy=-dy;
+        }
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        rec  = new Rectangle(xpos, ypos, width, height);
+
+    }
+
+
+
+    public void wrap(){
+
+        if(xpos>1000) {
+            xpos = 0;
+
+        }
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        if(xpos<0){
+            xpos = 1000;
+            xpos = xpos + dx;
+            ypos = ypos + dy;
+        }
+
+        if(ypos>700){
+            ypos = 0;
+            xpos = xpos + dx;
+            ypos = ypos + dy;
+        }
+
+        if(ypos<0){
+            ypos = 700;
+            xpos = xpos + dx;
+            ypos = ypos + dy;
+        }
+        rec  = new Rectangle(xpos, ypos, width, height);
+    }
+
+
+
+
+}
